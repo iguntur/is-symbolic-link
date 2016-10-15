6bfca4302ce35426a663c8969de8c0b7790516d9
Guntur <poetra.guntur@gmail.com>
2016-10-16 00:29:08 +0700

    common js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
-const fs = require('fs');
+var fs = require('fs');
 
-module.exports = path => new Promise(resolve => {
-	fs.lstat(path, (err, stats) => {
-		if (err) {
-			throw err;
-		}
+module.exports = function (path) {
+	return new Promise(function (resolve) {
+		fs.lstat(path, function (err, stats) {
+			if (err) {
+				throw err;
+			}
 
-		resolve(stats.isSymbolicLink());
+			resolve(stats.isSymbolicLink());
+		});
 	});
-});
+};
 
-module.exports.sync = path => {
+module.exports.sync = function (path) {
 	try {
-		const stats = fs.lstatSync(path);
+		var stats = fs.lstatSync(path);
 
 		return stats.isSymbolicLink();
 	} catch (err) {
